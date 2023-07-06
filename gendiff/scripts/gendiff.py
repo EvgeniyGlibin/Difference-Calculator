85ab236c6363a03efbb478c962a5e7090ed25dd0
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-07-06 11:35:38 +0900

    add help information on the gendiff utility

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
-def main():
-    print('Работает')
+import argparse
 
 
-if __name__ == '__main__':
-    main()
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+args = parser.parse_args()
