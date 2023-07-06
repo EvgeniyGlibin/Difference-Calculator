4b2d09a163ad70cf1542f91b0ee2d44838cfe4f8
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-07-06 12:28:05 +0900

    fix linter errors

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,9 @@
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser = argparse.ArgumentParser(
+    description='Compares two configuration files and shows a difference.'
+    )
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 args = parser.parse_args()
