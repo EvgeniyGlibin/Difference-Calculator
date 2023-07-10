3ff2daf5cc8dbbec405295134f3a6e6ac04b7abf
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-07-10 18:38:16 +0900

    update file gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+
+import itertools
 import argparse
 import json
 
@@ -10,8 +14,12 @@ parser.add_argument('-f', '--format', help='set format of output')
 args = parser.parse_args()
 
 
-first_file = json.load(open('file1.json'))
-second_file = json.load(open('file2.json'))
+first_file = json.load(open(
+    '/home/user/python-project-50/gendiff/file1.json'
+    ))
+second_file = json.load(open(
+    '/home/user/python-project-50/gendiff/file2.json'
+    ))
 
 
 def index_first_symbol(string):
@@ -22,12 +30,12 @@ def index_first_symbol(string):
 
 
 def generate_diff(file_path1, file_path2):
-    
+
     diff_file = {}
     for i in file_path1:
         if i in file_path2:
             if file_path1[i] == file_path2[i]:
-                diff_file[i] = file_path1[i]
+                diff_file[f'  {i}'] = file_path1[i]
             else:
                 diff_file[f'- {i}'] = file_path1[i]
                 diff_file[f'+ {i}'] = file_path2[i]
@@ -38,13 +46,33 @@ def generate_diff(file_path1, file_path2):
         if i not in file_path1:
             diff_file[f'+ {i}'] = file_path2[i]
 
-
-    sorted_diff_file = sorted(diff_file.items(), key=lambda x: x[0][index_first_symbol(x[0])])
+    sorted_diff_file = sorted(
+        diff_file.items(), key=lambda x: x[0][index_first_symbol(x[0])]
+        )
     return dict(sorted_diff_file)
+
+
+def stringify(value, replacer=' ', spaces_count=1):
+
+    def iter_(current_value, depth):
+        if not isinstance(current_value, dict):
+            return str(current_value)
+
+        deep_indent_size = depth + spaces_count
+        deep_indent = replacer * deep_indent_size
+        current_indent = replacer * depth
+        lines = []
+        for key, val in current_value.items():
+            lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
+        result = itertools.chain("{", lines, [current_indent + "}"])
+        return '\n'.join(result)
+
+    return iter_(value, 0)
+
 
 def main():
     diff = generate_diff(first_file, second_file)
-    print(diff)
+    print(stringify(diff))
 
 
 if __name__ == "__main__":
