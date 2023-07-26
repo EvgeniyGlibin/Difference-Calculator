2e7e29da7af563bf0c348b62fbf0f4d145a006bc
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-07-26 10:13:05 +0900

    update functions diff_json

--- gendiff/diff_files/diff_json.py
+++ gendiff/diff_files/diff_json.py
@@ -2,12 +2,6 @@ import itertools
 from types import NoneType
 import json
 
-def index_first_symbol(string):
-    for i in string:
-        if i.isalpha():
-            index = string.index(i)
-            return index
-
 
 def boolean_to_string(dictionary):
     for key in dictionary:
@@ -16,28 +10,20 @@ def boolean_to_string(dictionary):
     return dictionary
 
 
-def generate_diff(file_path1, file_path2):
-
-    diff_file = {}
-    for i in file_path1:
-        if i in file_path2:
-            if file_path1[i] == file_path2[i]:
-                diff_file[f'  {i}'] = file_path1[i]
-            else:
-                diff_file[f'- {i}'] = file_path1[i]
-                diff_file[f'+ {i}'] = file_path2[i]
+def generate_diff(data1, data2):
+    keys = sorted(data1.keys() | data2.keys())
+    result = {}
+    for key in keys:
+        if key not in data1:
+            result[f'+ {key}'] = data2[key]
+        elif key not in data2:
+            result[f'- {key}'] = data1[key]
+        elif data1[key] == data2[key]:
+            result[f'  {key}'] = data1[key]
         else:
-            diff_file[f'- {i}'] = file_path1[i]
-
-    for i in file_path2:
-        if i not in file_path1:
-            diff_file[f'+ {i}'] = file_path2[i]
-
-    diff_file = boolean_to_string(diff_file)
-    sorted_diff_file = sorted(
-        diff_file.items(), key=lambda x: x[0][index_first_symbol(x[0])]
-        )
-    return dict(sorted_diff_file)
+            result[f'- {key}'] = data1[key]
+            result[f'+ {key}'] = data2[key]
+    return boolean_to_string(result)
 
 
 def stringify(value, replacer=' ', spaces_count=1):
