1cee40e113bc82ed6097c35d8bd013a8851eef07
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-07-27 11:16:56 +0900

    fix tests

--- tests/test_diff_json.py
+++ tests/test_diff_json.py
@@ -1,6 +1,5 @@
 from gendiff.diff_files.diff_json import generate_diff
 from gendiff.diff_files.diff_json import stringify
-from gendiff.diff_files.diff_json import boolean_to_string
 import os
 
 
@@ -117,46 +116,63 @@ plain_data = read(get_fixture_path('plain.txt')).rstrip().split('\n\n\n')
 nested_data = read(get_fixture_path('nested.txt')).rstrip().split('\n\n\n')
 gen_diff = generate_diff(file1, file2)
 flat_json_1 = read(get_fixture_path('flat_json.txt')).rstrip().split('\n\n\n')
-
-
+print(stringify(gen_diff))
+print(flat_json_1[0])
 def test_default_values():
     assert stringify(primitives) == plain_data[2]
     assert stringify(primitives, '|-') == plain_data[0]
     assert stringify(primitives, '|-', 2) == plain_data[1]
-    assert stringify(nested) == nested_data[1]
-    assert stringify(nested, ' ') == nested_data[1]
-    assert stringify(nested, '...') == nested_data[0]
+    assert stringify(nested, ' ', 1) == nested_data[1]
+    assert stringify(nested, '...', 1) == nested_data[0]
     assert stringify(nested, '|-', 2) == nested_data[2]
     assert stringify(gen_diff) == flat_json_1[0]
 
 
 def test_generate_diff():
     assert generate_diff(file1, file2) == {
-        '- follow': 'false',
+        '- follow': False,
         '  host': 'hexlet.io',
         '- proxy': '123.234.53.22',
         '- timeout': 50,
         '+ timeout': 20,
-        '+ verbose': 'true',
+        '+ verbose': True,
         }
-    # assert generate_diff(file3, file4) == {
-
-    # }
-
-
-def test_boolean_to_string():
-    assert boolean_to_string(primitives) == {
-        "string": "value",
-        "boolean": "true",
-        "number": 5,
-        }
-    assert boolean_to_string(file1) == {
-        "host": "hexlet.io",
-        "timeout": 50,
-        "proxy": "123.234.53.22",
-        "follow": "false",
-        }
-    assert boolean_to_string({"a": 1, "b": "aa"}) == {
-        "a": 1,
-        "b": "aa",
-        }
+    assert generate_diff(file3, file4) == {
+        '  common': {
+            '+ follow': False,
+            '  setting1': 'Value 1',
+            '- setting2': 200,
+            '- setting3': True,
+            '+ setting3': None,
+            '+ setting4': 'blah blah',
+            '+ setting5': {
+                'key5': 'value5'
+                },
+            '  setting6': {
+                '  doge': {
+                    '- wow': '',
+                    '+ wow': 'so much'
+                    },
+                '  key': 'value',
+                '+ ops': 'vops'
+                }},
+          '  group1': {
+              '- baz': 'bas',
+              '+ baz': 'bars',
+              '  foo': 'bar',
+              '- nest': {
+                  'key': 'value'
+                  },
+              '+ nest': 'str'},
+          '- group2': {
+              'abc': 12345,
+              'deep': {
+                  'id': 45
+                  }},
+          '+ group3': {
+              'deep': {
+                  'id': {
+                      'number': 45
+                  }},
+              'fee': 100500
+              }}
