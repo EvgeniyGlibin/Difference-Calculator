8b83c6e3707ba1de2e9e903a2c111d60c0b41084
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-07-31 06:30:02 +0900

    delete comments

--- gendiff/diff_files/diff_json.py
+++ gendiff/diff_files/diff_json.py
@@ -3,29 +3,6 @@ from types import NoneType
 import json
 
 
-# def boolean_to_string(dictionary):
-#     for key in dictionary:
-#         if isinstance(dictionary[key], NoneType | bool):
-#             dictionary[key] = json.dumps(dictionary[key])
-#     return dictionary
-
-
-# def generate_diff(data1, data2):
-#     keys = sorted(data1.keys() | data2.keys())
-#     result = {}
-#     for key in keys:
-#         if key not in data1:
-#             result[f'+ {key}'] = data2[key]
-#         elif key not in data2:
-#             result[f'- {key}'] = data1[key]
-#         elif data1[key] == data2[key]:
-#             result[f'  {key}'] = data1[key]
-#         else:
-#             result[f'- {key}'] = data1[key]
-#             result[f'+ {key}'] = data2[key]
-#     return boolean_to_string(result)
-
-
 def stringify(value, replacer=' ', spaces_count=4):
 
     def iter_(current_value, depth):
