203f9b56a079ec61af9e1b417c61c7b93493f205
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-08-04 07:29:09 +0900

    update and add tests

--- tests/test_diff_json.py
+++ tests/test_diff_json.py
@@ -1,5 +1,6 @@
 from gendiff.diff_files.diff_json import generate_diff
 from gendiff.diff_files.stylish import stringify
+from gendiff.diff_files.plain import get_plain_formater
 import os
 
 
@@ -8,72 +9,76 @@ file1 = {
     "timeout": 50,
     "proxy": "123.234.53.22",
     "follow": False
-  }
+}
 
 
 file2 = {
     "timeout": 20,
     "verbose": True,
     "host": "hexlet.io"
-  }
+}
+
 
 file3 = {
-  "common": {
-    "setting1": "Value 1",
-    "setting2": 200,
-    "setting3": True,
-    "setting6": {
-      "key": "value",
-      "doge": {
-        "wow": "",
-      }
+    "common": {
+        "setting1": "Value 1",
+        "setting2": 200,
+        "setting3": True,
+        "setting6": {
+            "key": "value",
+            "doge": {
+                "wow": "",
+            }
+        }
+    },
+    "group1": {
+        "baz": "bas",
+        "foo": "bar",
+        "nest": {
+            "key": "value",
+        }
+    },
+    "group2": {
+        "abc": 12345,
+        "deep": {
+            "id": 45,
+        }
     }
-  },
-  "group1": {
-    "baz": "bas",
-    "foo": "bar",
-    "nest": {
-      "key": "value",
-    }
-  },
-  "group2": {
-    "abc": 12345,
-    "deep": {
-      "id": 45,
-    }
-  }
 }
+
+
 file4 = {
-  "common": {
-    "follow": False,
-    "setting1": "Value 1",
-    "setting3": None,
-    "setting4": "blah blah",
-    "setting5": {
-      "key5": "value5",
+    "common": {
+        "follow": False,
+        "setting1": "Value 1",
+        "setting3": None,
+        "setting4": "blah blah",
+        "setting5": {
+            "key5": "value5",
+        },
+        "setting6": {
+            "key": "value",
+            "ops": "vops",
+            "doge": {
+                "wow": "so much",
+            }
+        }
     },
-    "setting6": {
-      "key": "value",
-      "ops": "vops",
-      "doge": {
-        "wow": "so much",
-      }
+    "group1": {
+        "foo": "bar",
+        "baz": "bars",
+        "nest": "str",
+    },
+    "group3": {
+        "deep": {
+            "id": {
+                "number": 45,
+            }
+        },
+        "fee": 100500,
     }
-  },
-  "group1": {
-    "foo": "bar",
-    "baz": "bars",
-    "nest": "str",
-  },
-  "group3": {
-    "deep": {
-      "id": {
-        "number": 45,
-      }
-    },
-    "fee": 100500,
-  }
 }
+
 
 primitives = {
     "string": "value",
@@ -101,6 +106,36 @@ nested = {
 }
 
 
+file_diff = {
+    "- follow": "false",
+    "  host": "hexlet.io",
+    "- proxy": "123.234.53.22",
+    "- timeout": 50,
+    "+ timeout": 20,
+    "+ verbose": True,
+}
+
+
+plain = "Property 'follow' was removed\n" \
+    "Property 'proxy' was removed\n" \
+    "Property 'timeout' was updated. From 50 to 20\n" \
+    "Property 'verbose' was added with value: true" \
+
+
+plain_nested = "Property 'common.follow' was added with value: false\n" \
+    "Property 'common.setting2' was removed\n" \
+    "Property 'common.setting3' was updated. From true to null\n" \
+    "Property 'common.setting4' was added with value: 'blah blah'\n" \
+    "Property 'common.setting5' was added with value: [complex value]\n" \
+    "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'\n" \
+    "Property 'common.setting6.ops' was added with value: 'vops'\n" \
+    "Property 'group1.baz' was updated. From 'bas' to 'bars'\n" \
+    "Property 'group1.nest' was updated. From [complex value] to 'str'\n" \
+    "Property 'group2' was removed\n" \
+    "Property 'group3' was added with value: [complex value]" \
+
+
+
 def get_fixture_path(file_name):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, 'fixtures', file_name)
@@ -114,10 +149,9 @@ def read(file_path):
 
 plain_data = read(get_fixture_path('plain.txt')).rstrip().split('\n\n\n')
 nested_data = read(get_fixture_path('nested.txt')).rstrip().split('\n\n\n')
-gen_diff = generate_diff(file1, file2)
 flat_json_1 = read(get_fixture_path('flat_json.txt')).rstrip().split('\n\n\n')
-print(stringify(gen_diff))
-print(flat_json_1[0])
+
+
 def test_default_values():
     assert stringify(primitives) == plain_data[2]
     assert stringify(primitives, '|-') == plain_data[0]
@@ -125,54 +159,19 @@ def test_default_values():
     assert stringify(nested, ' ', 1) == nested_data[1]
     assert stringify(nested, '...', 1) == nested_data[0]
     assert stringify(nested, '|-', 2) == nested_data[2]
-    assert stringify(gen_diff) == flat_json_1[0]
+
+
+def test_get_plain_formater():
+    assert get_plain_formater(file_diff) == plain
 
 
 def test_generate_diff():
-    assert generate_diff(file1, file2) == {
-        '- follow': False,
-        '  host': 'hexlet.io',
-        '- proxy': '123.234.53.22',
-        '- timeout': 50,
-        '+ timeout': 20,
-        '+ verbose': True,
-        }
-    assert generate_diff(file3, file4) == {
-        '  common': {
-            '+ follow': False,
-            '  setting1': 'Value 1',
-            '- setting2': 200,
-            '- setting3': True,
-            '+ setting3': None,
-            '+ setting4': 'blah blah',
-            '+ setting5': {
-                'key5': 'value5'
-                },
-            '  setting6': {
-                '  doge': {
-                    '- wow': '',
-                    '+ wow': 'so much'
-                    },
-                '  key': 'value',
-                '+ ops': 'vops'
-                }},
-          '  group1': {
-              '- baz': 'bas',
-              '+ baz': 'bars',
-              '  foo': 'bar',
-              '- nest': {
-                  'key': 'value'
-                  },
-              '+ nest': 'str'},
-          '- group2': {
-              'abc': 12345,
-              'deep': {
-                  'id': 45
-                  }},
-          '+ group3': {
-              'deep': {
-                  'id': {
-                      'number': 45
-                  }},
-              'fee': 100500
-              }}
+    assert generate_diff(file1, file2, stringify) == "{\n" \
+        "  - follow: false\n" \
+        "    host: hexlet.io\n" \
+        "  - proxy: 123.234.53.22\n" \
+        "  - timeout: 50\n" \
+        "  + timeout: 20\n" \
+        "  + verbose: true\n" \
+        "}"
+    assert generate_diff(file3, file4, get_plain_formater) == plain_nested
