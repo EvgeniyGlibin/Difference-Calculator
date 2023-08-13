1de545cb84c8acf771ad3c22f5dcb4fdab50d323
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-08-14 07:57:58 +0900

    add file cli.py

---
+++ gendiff/cli.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import json
+from gendiff.diff_files.stylish import stringify
+from gendiff.diff_files.plain import get_plain_formater
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', default='stylish',
+                        help='set format of output')
+    args = parser.parse_args()
+    if args.format == 'stylish':
+        format_name = stringify
+    elif args.format == 'plain':
+        format_name = get_plain_formater
+    elif args.format == 'json':
+        format_name = json.dumps
+
+    return args, format_name
+
+
+def main():
+    get_parser()
+
+
+if __name__ == "__main__":
+    main()
