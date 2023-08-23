b4727eaa9311e7bd51320c17d0cfa731b044d3c8
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-08-23 17:10:48 +0900

    delete comments

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,23 +4,6 @@ from gendiff.diff_generator import generate_diff
 
 
 args, format_name = get_parser()
-# file_path1 = '/home/user/python-project-50/gendiff/file1'
-# file_path2 = '/home/user/python-project-50/gendiff/file2'
-
-
-# if args.first_file.endswith(".json"):
-#     first_file = f'{file_path1}.json'
-# elif args.first_file.endswith((".yaml", ".yml")):
-#     first_file = f'{file_path1}.yaml'
-# else:
-#     print("Нет такого файла")
-
-# if args.second_file.endswith(".json"):
-#     second_file = f'{file_path2}.json'
-# elif args.second_file.endswith((".yaml", ".yml")):
-#     second_file = f'{file_path2}.yaml'
-# else:
-#     print("Нет такого файла")
 
 
 def main():
