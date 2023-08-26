2af843fb571a382054a4649ee605cbc81103965f
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-08-26 11:36:09 +0900

    fix bugs lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,9 +3,6 @@ from gendiff.cli import get_parser
 from gendiff.diff_generator import generate_diff
 
 
-
-
-
 def main():
     args, format_name = get_parser()
     diff = generate_diff(args.first_file, args.second_file, format_name)
