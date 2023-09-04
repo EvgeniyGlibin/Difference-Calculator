83ae0bb3c4700ce29ecb235dcf5eb399722411dc
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-09-04 17:22:34 +0900

    fix bugs lint

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -13,8 +13,8 @@ def get_parser():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', 
-                        default='stylish', 
+    parser.add_argument('-f', '--format',
+                        default='stylish',
                         choices=['stylish', 'plain', 'json'],
                         help='set format of output')
     args = parser.parse_args()
