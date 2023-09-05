cf6b0602bb56781aa03e721b896b0c04d99e626c
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-09-06 07:43:12 +0900

    add OUTPUT_FORMAT variable

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -6,6 +6,19 @@ from gendiff.diff_files.formats.json import get_json
 from gendiff.diff_files.gener_diff import generate_result
 
 
+FILE_FORMAT = {
+    "json": json.loads,
+    "yaml": yaml.safe_load,
+    "yml": yaml.safe_load,
+}
+
+OUTPUT_FORMAT = {
+    "stylish": stringify,
+    "plain": get_plain_formater,
+    "json": get_json,
+}
+
+
 def read(file_path):
     with open(file_path, 'r') as f:
         result = f.read()
@@ -16,24 +29,9 @@ def get_format_files(path):
     return path.split('.')[-1]
 
 
-FORMATS = {
-    "json": json.loads,
-    "yaml": yaml.safe_load,
-    "yml": yaml.safe_load,
-}
-
-
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    first_file = FORMATS[get_format_files(file_path1)](read(file_path1))
-    second_file = FORMATS[get_format_files(file_path2)](read(file_path2))
+    first_file = FILE_FORMAT[get_format_files(file_path1)](read(file_path1))
+    second_file = FILE_FORMAT[get_format_files(file_path2)](read(file_path2))
 
     result = generate_result(first_file, second_file)
-
-    if format_name == 'stylish':
-        format_name = stringify
-    elif format_name == 'plain':
-        format_name = get_plain_formater
-    elif format_name == 'json':
-        format_name = get_json
-
-    return format_name(result)
+    return OUTPUT_FORMAT[format_name](result)
