c6af842d6128e39ea138fa4d9e63aa6e6bc6cf4e
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-09-06 07:41:40 +0900

    remove references to formats

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,9 +2,6 @@
 
 
 import argparse
-import json
-from gendiff.diff_files.formats.stylish import stringify
-from gendiff.diff_files.formats.plain import get_plain_formater
 
 
 def get_parser():
@@ -18,14 +15,8 @@ def get_parser():
                         choices=['stylish', 'plain', 'json'],
                         help='set format of output')
     args = parser.parse_args()
-    if args.format == 'stylish':
-        format_name = stringify
-    elif args.format == 'plain':
-        format_name = get_plain_formater
-    elif args.format == 'json':
-        format_name = json.dumps
 
-    return args, format_name
+    return args
 
 
 def main():
