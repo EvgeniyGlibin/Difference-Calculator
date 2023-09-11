cb3b9e60ed18787882fa18c961948b3c526f6224
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-09-12 07:43:09 +0900

    improve the "stylish" function

--- gendiff/diff_files/gener_diff.py
+++ gendiff/diff_files/gener_diff.py
@@ -63,8 +63,73 @@ def generate_result(data1, data2):
 # type_operation:[added, change ....], new: ..., old:...
 
 
-path_file1_json = "/home/evgeniy/python-project-50/tests/fixtures/file1.json"
-path_file2_json = "/home/evgeniy/python-project-50/tests/fixtures/file2.json"
-first_file = json.loads(read(path_file1_json))
-second_file = json.loads(read(path_file2_json))
-print(generate_result(first_file, second_file))
+# path_file1_json = "/home/evgeniy/python-project-50/tests/fixtures/file1.json"
+# path_file2_json = "/home/evgeniy/python-project-50/tests/fixtures/file2.json"
+# first_file = json.loads(read(path_file1_json))
+# second_file = json.loads(read(path_file2_json))
+# x = (generate_result(first_file, second_file))
+
+# print('----------------------------')
+# for i in x:
+#     print(i, sep='\n')
+
+
+data1 = {
+    "follow": False,
+    "host": "hexlet.io",
+    "proxy": "123.234.53.22",
+    "timeout": 50,
+    }
+
+data2 = {
+    "host": 'hexlet.io',
+    "proxy": "123.234.53.45",
+    "timeout": 25,
+    "promt": True
+}
+
+gen_diff = generate_result(data1, data2)
+print(gen_diff)
+print('-------------------')
+# dictionary = gen_diff[0]
+# print(dictionary)
+# for key, val in dictionary.items():
+#     print(key, val, sep=' -- ')
+import itertools
+from types import NoneType
+import json
+
+
+def stringify(value, replacer=' ', spaces_count=4):
+
+    def iter_(current_value, depth):
+        for dictionary in current_value:
+            if not isinstance(dictionary, dict):
+                return str(dictionary)
+
+            deep_indent_size = depth + spaces_count
+            deep_indent = replacer * deep_indent_size
+            current_indent = replacer * depth
+            lines = []
+            key = dictionary['key']
+            val = str(dictionary['old_value'])
+            if dictionary['type_operation'] in ['added', 'remove', 'changed', 'unchanged']:
+                deep_indent = replacer * (deep_indent_size - 2)
+                lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
+            result = itertools.chain("{", lines, [current_indent + "}"])
+            return '\n'.join(result)
+
+            # for key, val in dictionary.items():
+            #     if isinstance(val, NoneType | bool):
+            #         val = json.dumps(val)
+            #     if str(key)[0] in "+- ":
+            #         deep_indent = replacer * (deep_indent_size - 2)
+            #     lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
+            # result = itertools.chain("{", lines, [current_indent + "}"])
+            # return '\n'.join(result)
+
+    return iter_(value, 0)
+
+
+string = stringify(gen_diff)
+print(string)
